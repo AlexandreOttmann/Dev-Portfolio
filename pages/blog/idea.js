edc36c86b3c91d171fc92a6481d97d012b3fadea
AlexandreOttmann <ottmann.alex@gmail.com>
2023-03-01 03:39:13 +0100

    solved extension issue

--- pages/blog/idea.js
+++ pages/blog/idea.js
@@ -34,7 +34,7 @@ const Work = () => {
       </P>
       <SimpleGrid gap={4} minChildWidth='350px' className="tw-items-center tw-mt-4">
 
-        <WorkImage src="/images/works/idea_02.jpg" alt="learning picture" />
+        <WorkImage src="/images/works/idea_02.JPG" alt="learning picture" />
         <P className=" ">
           They are not building shinny portfolio but <em className="tw-text-teal-500">stable architectures</em>. This is what this portfolio want to display.
           It is built with React and Next.Js, using ChakraUI for the styling and Tailwind over it for fine tuning. Deliberately simple and minimalistic. I want to show a fully functionnal, responsive and beautiful portfolio, emphasizing on the content, showing what I can do for now and showing who I am too. And particulary displaying my capacity to learn.</P>
@@ -44,7 +44,7 @@ const Work = () => {
       <SimpleGrid gap={4} minChildWidth='350px' className="tw-items-center tw-mt-4">
         <P>3 months behind I wasn&apos;t able to do a single Algorithm, nor build a single HTML web page. And I&apos;m now digging React, creating my own portfolio. I discovered those last months a big desire to learn and spend most of my time for it. I think it&apos;s a really big part of what make me think that I made the right choice, learning web development.
         </P>
-        <WorkImage src="/images/works/idea_03.jpg" alt="learning picture" />
+        <WorkImage src="/images/works/idea_03.JPG" alt="learning picture" />
       </SimpleGrid>
       <span className='tw-flex tw-w-full tw-justify-center tw-mb-10'><P className="tw-text-xl">Set <strong className="tw-text-orange-500">Sail !</strong></P></span>
 
