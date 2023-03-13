dab8d2ba87034bf1166f6317745d4b79de6dce14
AlexandreOttmann <ottmann.alex@gmail.com>
2023-03-13 13:05:41 +0100

    Add Loading Page

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -2,6 +2,8 @@
 import { motion } from 'framer-motion'
 import Head from 'next/head'
 import { GridItemStyle } from '../grid-items'
+import { Suspense } from 'react'
+import Loading from './loading.js'
 
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
@@ -14,18 +16,21 @@ const Layout = ({ children, title }) => {
   const t = `${title} - Alexandre Ottmann`
 
   return (
-    <motion.article initial="hidden" animate="enter" exit="exit" variants={variants} transition={{ duration: 0.4, type: 'easeInOut' }} style={{ position: 'relative' }}>
-      <>
-        {title && (
-          <Head>
-            <title>{t}</title>
-          </Head>
-        )}
-        {children}
-        <GridItemStyle />
+    <Suspense fallback={<Loading />}>
 
-      </>
-    </motion.article>
+      <motion.article initial="hidden" animate="enter" exit="exit" variants={variants} transition={{ duration: 0.4, type: 'easeInOut' }} style={{ position: 'relative' }}>
+        <>
+          {title && (
+            <Head>
+              <title>{t}</title>
+            </Head>
+          )}
+          {children}
+          <GridItemStyle />
+
+        </>
+      </motion.article>
+    </Suspense>
   )
 }
 
--- components/layouts/main.js
+++ components/layouts/main.js
@@ -3,39 +3,43 @@ import Navbar from '../navbar.js'
 import VideoHeader from '../videoHeader.js'
 import { Box, Container } from '@chakra-ui/react'
 import Footer from '../footer'
+import { Suspense } from 'react'
+import Loading from './loading.js'
 
 const Main = ({ children, router }) => {
 
   const param = router.asPath
 
   return (
-    <Box as="main" pb={8}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Alex's homepage" />
-        <meta name="author" content="Alexandre Ottmann" />
+    <Suspense fallback={<Loading />}>
+      <Box as="main" pb={8}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="Alex's homepage" />
+          <meta name="author" content="Alexandre Ottmann" />
 
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 
-        <title>Alexandre Ottmann - Page d&apos;accueil</title>
-      </Head>
+          <title>Alexandre Ottmann - Page d&apos;accueil</title>
+        </Head>
 
-      <Navbar path={router.asPath} className="tw-z-20" />
-      {param == '/blog' &&
-        <VideoHeader />
-      }
+        <Navbar path={router.asPath} className="tw-z-20" />
+        {param == '/blog' &&
+          <VideoHeader />
+        }
 
-      <Container
+        <Container
 
-        maxW="container.md"
-        pt={14}>
+          maxW="container.md"
+          pt={14}>
 
-        {children}
+          {children}
 
-        <Footer />
-      </Container>
-    </Box >
+          <Footer />
+        </Container>
+      </Box >
+    </Suspense>
   )
 }
 
